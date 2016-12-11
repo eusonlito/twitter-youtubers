4cc598cd8715e5d29e326add33c1f3d175ab9166
Lito <lito@eordes.com>
2016-12-11 12:09:36 +0100

    Added viewport header tags

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,6 +1,10 @@
 <!DOCTYPE HTML>
-<html lang="es">
+<html lang="en">
     <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
         <link href="//getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet" />
 
